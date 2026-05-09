906e2d00a4b8668dfe8710b7aeaba812d779c3a3
c3spinoza <74637956+c3spinoza@users.noreply.github.com>
2026-05-08 21:24:54 -0400

    feat(order_dispatcher): added initial shift-register based fifo to output order to heap engines.

--- hw/sys_def.svh
+++ hw/sys_def.svh
@@ -16,17 +16,41 @@
 `timescale 1ns/100ps
 
 // Word width
-`define WORD_WIDTH = 32
+`define WORD_WIDTH 32
+
+// Order Dispatch Parameters
+`define SYM_NUM 8       // Number of symbols 
+`define FIFO_SZ 1660  // 
 
 // Structs 
+/*
+* Main Order Struct
+*/
 typedef struct packed {
     logic        type_;     // Ask (0) or Bid (1)
     logic [15:0] price;     // unsigned int in [0, 65,536]
     logic [15:0] quantity;  // unsigned int in [0, 65,536]
     logic [20:0] symbol;    // 3 ASCII uppercase char (7-bit each)
     logic [31:0] timestamp; // unsigned int in [0, 4,294,967,296]
-    logic [9:0]  pad;       // Padding to word width
 } ORDER;
 
+/*
+* The orders stored in the dispatch:
+*  - This is was is written to the input FIFOs
+*  - This is was is passed from the Dipatcher to the engines.  
+*/
+typedef struct packed {
+    logic        type_;     // Ask (0) or Bid (1)
+    logic [15:0] price;     // unsigned int in [0, 65,536]
+    logic [14:0] quantity;  // unsigned int in [0, 65,536]
+
+} DISPATCH_ORDER;
+
+typedef enum logic [1:0] {
+  IDLE      = 2'd0,     // Doing nothing
+  WRITE     = 2'd1,     // Waiting for harness to finish writing
+  DISPATCH  = 2'd2,     // Dispatching orders
+  DONE      = 2'd3      // Done dispatching
+} DISPATCH_STATE;
 
 `endif // __SYS_DEFS_SVH__
